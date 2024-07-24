4abbe3d9ff0f0d8bd75c066e3f95efd3e2cff450
yyx <3247828719@qq.com>
2024-07-24 23:08:55 +0800

    通过nacos获取项目配置

--- go.mod
+++ go.mod
@@ -9,16 +9,26 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/alibabacloud-go/debug v0.0.0-20190504072949-9472017b5c68 // indirect
 	github.com/alibabacloud-go/dysmsapi-20170525/v3 v3.0.6 // indirect
+	github.com/alibabacloud-go/tea v1.1.19 // indirect
+	github.com/alibabacloud-go/tea-utils v1.4.4 // indirect
 	github.com/aliyun/alibaba-cloud-sdk-go v1.62.785 // indirect
+	github.com/aliyun/alibabacloud-dkms-gcs-go-sdk v0.2.2 // indirect
+	github.com/aliyun/alibabacloud-dkms-transfer-go-sdk v0.1.7 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/lestrrat-go/strftime v1.0.6 // indirect
 	github.com/lionsoul2014/ip2region/binding/golang v0.0.0-20240510055607-89e20ab7b6c6 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/opentracing/opentracing-go v1.2.1-0.20220228012449-10b1cf09e00b // indirect
@@ -26,8 +36,15 @@ require (
 	github.com/oschwald/maxminddb-golang v1.13.0 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.12.2 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.32.1 // indirect
+	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/redis/go-redis/v9 v9.5.3 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/time v0.1.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gorm.io/driver/mysql v1.5.7 // indirect
 )
 
@@ -36,6 +53,7 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/nacos-group/nacos-sdk-go/v2 v2.2.7
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.24.0
 	golang.org/x/net v0.26.0 // indirect
@@ -44,5 +62,6 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240610135401-a8a62080eff3 // indirect
 	google.golang.org/grpc v1.64.0
 	google.golang.org/protobuf v1.34.2
+	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,11 +3,16 @@ package config
 import (
 	"context"
 	"fmt"
-	"os"
+	"io/ioutil"
+	"net/http"
 
+	"github.com/magiconair/properties"
+	"github.com/nacos-group/nacos-sdk-go/v2/clients"
+	"github.com/nacos-group/nacos-sdk-go/v2/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/v2/vo"
 	"github.com/nepu-SidneyYu/blog-grpc/internal/logs"
 	"go.uber.org/zap"
-	"gopkg.in/yaml.v3"
+	"gopkg.in/yaml.v2"
 )
 
 // Config结构体，包含MySqlConfig字段
@@ -21,9 +26,28 @@ type Config struct {
 	Email SendEmailCodeConfig `yaml:"email"`
 	Phone SendPhoneCodeConfig `yaml:"phone"`
 }
+type NacosConfig struct {
+	TimeoutMs            uint64 `yaml:"timeout_ms"`              // 超时时间
+	NamespaceId          string `yaml:"namespace_id"`            // 命名空间
+	CacheDir             string `yaml:"cache_dir"`               // 缓存目录
+	NotLoadCacheAtStart  bool   `yaml:"not_load_cache_at_start"` // 是否在启动时加载缓存
+	UpdateCacheWhenEmpty bool   `yaml:"update_cache_when_empty"` // 是否在缓存为空时更新缓存
+	Username             string `yaml:"username"`                // 用户名
+	Password             string `yaml:"password"`                // 密码
+	LogDir               string `yaml:"log_dir"`                 // 日志目录
+	LogLevel             string `yaml:"log_level"`               // 日志级别
+	ContextPath          string `yaml:"context_path"`            // 上下文路径
+	Scheme               string `yaml:"scheme"`                  // 协议
+	IpAddr               string `yaml:"ip_addr"`                 // ip地址
+	Port                 uint64 `yaml:"port"`                    // 端口
+	DataType             string `yaml:"data_type"`               // 数据类型
+	DataId               string `yaml:"data_id"`                 // 数据id
+	Group                string `yaml:"group"`                   // 分组
+}
 
 var (
 	_config Config
+	_nacos  NacosConfig
 )
 
 type SendPhoneCodeConfig struct {
@@ -67,16 +91,85 @@ type RedisConfig struct {
 	DB       int    `yaml:"db"`
 }
 
+func loadNacosConfig() {
+	// TODO: 从nacos加载配置
+	resp, err := http.Get("http://conf.zhihuishu.com/global/nacos.properties")
+	if err != nil {
+		logs.Fatal(context.Background(), "获取Nacos配置信息失败", zap.String("error", err.Error()))
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logs.Fatal(context.Background(), "读取Nacos配置信息失败", zap.String("error", err.Error()))
+	}
+	p, err := properties.Load(body, properties.UTF8)
+	if err != nil {
+		logs.Fatal(context.Background(), "解析Nacos配置信息失败", zap.String("error", err.Error()))
+	}
+	username := p.GetString("nacos.zhihuishu.server.username")
+	password := p.GetString("nacos.zhihuishu.server.password")
+	if username == "" || password == "" {
+		logs.Fatal(context.Background(), "Nacos配置信息不完整", zap.String("error", "nacos.zhihuishu.server.username or nacos.zhihuishu.server.password is empty"))
+	}
+	_nacos = NacosConfig{Username: username, Password: password}
+	logs.Info(context.Background(), "加载Nacos配置信息成功", zap.String("username", username), zap.String("password", password))
+}
+
 func LoadConfig() {
-	data, err := os.ReadFile("conf.yaml")
-	if err != nil {
-		logs.Fatal(context.Background(), "read yaml failed", zap.String("error", err.Error()))
+	loadNacosConfig()
+
+	clientConfig := &constant.ClientConfig{
+		NamespaceId:          "pract",
+		NotLoadCacheAtStart:  true,
+		LogDir:               "/tmp/nacos/log",
+		CacheDir:             "/tmp/nacos/cache",
+		TimeoutMs:            5000,
+		Username:             _nacos.Username,
+		Password:             _nacos.Password,
+		UpdateCacheWhenEmpty: true,
 	}
-	fmt.Printf("%s\n", string(data))
-	err = yaml.Unmarshal(data, &_config)
-	if err != nil {
-		logs.Fatal(context.Background(), "unmarshal yaml failed", zap.String("error", err.Error()))
+	serverConfigs := []constant.ServerConfig{
+		{
+			//ContextPath: "/nacos",
+			IpAddr: "nacos-i.zhihuishu.com",
+			Port:   8848,
+			Scheme: "http",
+		},
 	}
+	configClient, err := clients.NewConfigClient(
+		vo.ConfigParam{
+			ClientConfig:  clientConfig,
+			ServerConfigs: serverConfigs,
+		},
+	)
+	if err != nil {
+		logs.Fatal(context.Background(), "获取项目启动配置信息失败", zap.String("error", err.Error()))
+	}
+	content, err := configClient.GetConfig(vo.ConfigParam{
+		DataId: "config.yaml",
+		Group:  "config",
+		Type:   "yaml",
+	})
+	if err != nil {
+		logs.Fatal(context.Background(), "获取项目启动配置信息失败", zap.String("error", err.Error()))
+	}
+	logs.Info(context.Background(), "项目配置", zap.String("content", fmt.Sprintf("%s\n", content)))
+
+	err = yaml.Unmarshal([]byte(content), &_config)
+	if err != nil {
+		logs.Fatal(context.Background(), "解析项目启动配置信息失败", zap.String("error", err.Error()))
+	}
+	logs.Info(context.Background(), "加载项目启动配置信息成功", zap.String("_config", fmt.Sprintf("%s\n", _config)))
+
+	// data, err := os.ReadFile("conf.yaml")
+	// if err != nil {
+	// 	logs.Fatal(context.Background(), "read yaml failed", zap.String("error", err.Error()))
+	// }
+	// fmt.Printf("%s\n", string(data))
+	// err = yaml.Unmarshal(data, &_config)
+	// if err != nil {
+	// 	logs.Fatal(context.Background(), "unmarshal yaml failed", zap.String("error", err.Error()))
+	// }
 }
 
 func GetConfig() Config {
